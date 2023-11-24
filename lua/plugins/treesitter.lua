3183ff4c5cfd8175c0aaf41863d22d4d504a2e6d
Danila Skripko <dalas.prog@gmail.com>
2023-11-24 09:56:35 +0400

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "typescript", "javascript", "python", "cpp" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-	  enable = true,
-  },
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "lua", "typescript", "javascript" },
+	sync_install = false,
+	auto_install = true,
+	highlight = { enable = true },
+})
