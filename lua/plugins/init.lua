4c95715007b7ec7471d99e7a77d864dfad53b071
Danila Skripko <dalas.prog@gmail.com>
2024-08-19 10:25:00 +0400

    upd

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -100,4 +100,13 @@ return {
     "rust-lang/rust.vim",
     lazy = false,
   },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require "configs.catpuccin"
+    end,
+  },
 }
